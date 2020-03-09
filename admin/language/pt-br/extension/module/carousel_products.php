bfba9e214fe84af57fa79cb90da3b994ed0c2762
romulorodrigues <romulo@10envolve.com.br>
2020-03-09 15:53:50 -0300

    initial commit

---
+++ admin/language/pt-br/extension/module/carousel_products.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']    = 'Carrossel de Produtos';
+
+// Text
+$_['text_extension']   = 'Extensões';
+$_['text_success']     = 'Produtos em destaque modificado com sucesso!';
+$_['text_edit']        = 'Editando Produtos em destaque';
+
+// Entry
+$_['entry_name']       = 'Título do módulo';
+$_['entry_product']    = 'Produtos';
+$_['entry_limit']      = 'Limite';
+$_['entry_width']      = 'Largura';
+$_['entry_height']     = 'Altura';
+$_['entry_slides_per_view']     = 'Slides por visualização';
+$_['entry_autoplay']     = 'Tempo autoplay';
+$_['entry_loop']     = 'Loop';
+$_['entry_status']     = 'Situação';
+
+// Help
+$_['help_product']     = '(Autocompletar)';
+
+// Error
+$_['error_permission'] = 'Atenção: Você não tem permissão para modificar a extensão Produtos em destaque!';
+$_['error_name']       = 'Título do módulo deve ter entre 3 e 64 caracteres!';
+$_['error_width']      = 'Largura é obrigatória!';
+$_['error_height']     = 'Altura é obrigatória!';
